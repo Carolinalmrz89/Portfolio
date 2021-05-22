6a7219530b719063aade1e2c50b44977091f05d9
Carolinalmrz89 <lais.carolina.almaraz89@gmail.com>
2021-05-22 19:39:31 -0300

    Validación de formulario

---
+++ js/index.js
@@ -0,0 +1,42 @@
+"use strict";
+
+//VALIDACIÓN DE FORMULARIO
+const inputName = document.getElementById("nombre");
+const inputLastName = document.getElementById("apellido");
+const inputEmail = document.getElementById("email");
+const validName = document.querySelector(".valid-name");
+const validLastName = document.querySelector(".valid-lastname");
+const validMail = document.querySelector(".valid-mail");
+const invalidMail = document.querySelector(".invalid-mail");
+
+inputName.addEventListener("input", () =>{
+    if(inputName.validity.valid){
+        validName.style.visibility = "visible";
+    }else{
+        validName.style.visibility = "hidden";
+    }
+});
+
+inputLastName.addEventListener("input", () =>{
+    if(inputLastName.validity.valid){
+        validLastName.style.visibility = "visible";
+    }else{
+        validLastName.style.visibility = "hidden";
+    }
+});
+
+inputEmail.addEventListener("input", () =>{
+    if(inputEmail.validity.valid){
+        validMail.style.visibility = "visible";
+    }else{
+        validMail.style.visibility = "hidden";
+    }
+});
+
+inputEmail.addEventListener("input", () =>{
+    if(inputEmail.validity.valid == false){
+        invalidMail.style.visibility = "visible";
+    }else{
+        invalidMail.style.visibility = "hidden";
+    }
+});
